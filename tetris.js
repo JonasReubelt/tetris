58e1288f6782f46b1b569e683b4c83307ba9173b
Tamas Gal <himself@tamasgal.com>
2017-07-07 12:37:51 +0200

    Fix I rotation

--- tetris.js
+++ tetris.js
@@ -293,19 +293,19 @@ function rotated(pos){
     return pos;
   }
   if (tetris.id=="I"){
-    if (pos[0] == [0, 0]){
+    if (pos[0][0] == 0 && pos[0][1] == 0){
       new_pos = [[-1, 1], [0, 1], [1, 1], [2, 1]];
       return new_pos;
     }
-    else if (pos[0] == [-1, 1]){
+    else if (pos[0][0] == -1 && pos[0][1] == 1){
       new_pos = [[1, 0], [1, 1], [1, 2], [1, 3]];
       return new_pos;
     }
-    else if (pos[0] == [1, 0]){
+    else if (pos[0][0] == 1 && pos[0][1] ==  0){
       new_pos = [[-1, 2], [0, 2], [1, 2], [2, 2]];
       return new_pos;
     }
-    else if (pos[0] == [-1, 2]){
+    else if (pos[0][0] == -1 && pos[0][1] == 2){
       new_pos = [[0, 0], [0, 1], [0, 2], [0, 3]];
       return new_pos;
     }
