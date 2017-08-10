7d6323dfc48afbcf9444632dd93d26355621ec02
dstransky <dstransky@km3net.de>
2017-08-10 17:34:22 +0200

    no more green stuff with scenario 8

--- tetris.js
+++ tetris.js
@@ -682,19 +682,19 @@ function game_over() {
 }
 
 function load_scenario(n) {
-    if(n == 8) {
-        new_tetris = function() {
-            next_tetris = {"id": "S", "x": 5, "y": -1, "pos": tetrii["S"], "o": 0};
-            tetris = {"id": "S", "x": 5, "y": -1, "pos": tetrii["S"], "o": 0};
-        }
-        check_scenario_completed = function() {
-            return false;
-        }
-        reset();
-        init();
-        matrix = zeros([blocks_y, blocks_x]);
-        return;
-    }
+    //if(n == 8) {
+    //    new_tetris = function() {
+    //        next_tetris = {"id": "S", "x": 5, "y": -1, "pos": tetrii["S"], "o": 0};
+    //        tetris = {"id": "S", "x": 5, "y": -1, "pos": tetrii["S"], "o": 0};
+    //    }
+    //    check_scenario_completed = function() {
+    //        return false;
+    //    }
+    //    reset();
+    //    init();
+    //    matrix = zeros([blocks_y, blocks_x]);
+    //    return;
+    //}
     if(n == 9) {
         new_tetris = function() {
             next_tetris = {"id": "T", "x": 5, "y": -1, "pos": tetrii["T"], "o": 0};
